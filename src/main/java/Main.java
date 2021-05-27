40e70622c0ffeb502a836ebef599ebf6bba45b89
Marcelo Santos <rocha.mer21@gmail.com>
2021-05-27 15:30:26 -0300

    iniciando o estudo em java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,10 +4,16 @@ public class Main {
     public static void main(String[] args) {
         double y = 32.325255;
 
+        String nome = "Marcelo";
+        int idade = 30;
+        double renda  = 1300.0;
+
         System.out.println(y);
         System.out.printf("%.4f%n", y);
         Locale.setDefault(Locale.US);
         System.out.printf("%.4f%n", y);
         System.out.println("RESULTADO = " + y + " METROS");
+        System.out.printf("RESULTADO = %.2f METROS%n", y);
+        System.out.printf("%s tem %d anos e ganha R$ %.2f reais%n", nome, idade, renda);
     }
 }
