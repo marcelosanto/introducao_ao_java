bf8656a496ca8f3281d6df762c0fbef688f00b16
Marcelo Santos <rocha.mer21@gmail.com>
2021-05-28 22:31:28 -0300

    feat: if, else if e else.

---
+++ src/main/java/estruturacondicional/main.java
@@ -0,0 +1,21 @@
+package estruturacondicional;
+
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int hora;
+
+        System.out.println("Quantas horas?");
+        hora = sc.nextInt();
+
+        if(hora < 12) {
+            System.out.println("Bom dia!");
+        } else if (hora > 12 && hora < 18) {
+            System.out.println("Boa tarde!");
+        } else {
+            System.out.println("Boa noite!");
+        }
+    }
+}
