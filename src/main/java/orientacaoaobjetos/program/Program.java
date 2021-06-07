9dc287372457993ba71cf2523b05006a87b2dcfe
Marcelo Santos <rocha.mer21@gmail.com>
2021-06-07 00:33:07 -0300

    feat: solucionando segundo problema

---
+++ src/main/java/orientacaoaobjetos/program/Program.java
@@ -0,0 +1,31 @@
+package orientacaoaobjetos.program;
+
+import orientacaoaobjetos.program.entities.Product;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        Product product = new Product();
+
+        System.out.println("Enter product data: ");
+
+        System.out.print("name: ");
+        product.name = sc.nextLine();
+
+        System.out.print("price: ");
+        product.price = sc.nextDouble();
+
+        System.out.print("Quantity in stock: ");
+        product.quantity = sc.nextInt();
+
+        System.out.println(product.toString());
+
+
+        sc.close();
+    }
+}
