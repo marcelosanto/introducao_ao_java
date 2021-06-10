50e9a7b5b27ff32f7329cc0ef4a1e75a99416a99
Marcelo Santos <rocha.mer21@gmail.com>
2021-06-10 10:39:02 -0300

    feat: enumerator

---
+++ src/main/java/enumeracoes/Enumeracoes.java
@@ -0,0 +1,17 @@
+package enumeracoes;
+
+import enumeracoes.entities.Order;
+import enumeracoes.entities.enums.OrderStatus;
+
+import java.util.Date;
+
+public class Enumeracoes {
+    public static void main(String[] args) {
+        Order order = new Order(1080, new Date(), OrderStatus.PEDDING_PAYMENT);
+        System.out.println(order);
+
+        OrderStatus os1 = OrderStatus.DELIVERED;
+        OrderStatus os2 = OrderStatus.valueOf("DELIVERED");
+        System.out.println(os1 + " - " + os2);
+    }
+}
---
+++ src/main/java/enumeracoes/entities/enums/OrderStatus.java
@@ -0,0 +1,8 @@
+package enumeracoes.entities.enums;
+
+public enum OrderStatus {
+    PEDDING_PAYMENT,
+    PROCESSING,
+    SHIPPED,
+    DELIVERED;
+}
