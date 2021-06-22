448e65a691adc7ad78a0a4281541642a2b955eaa
Marcelo Santos <rocha.mer21@gmail.com>
2021-06-22 16:25:55 -0300

    feat: try/catch - printStackTrace()

--- src/main/java/tryandcatch/Main.java
+++ src/main/java/tryandcatch/Main.java
@@ -5,6 +5,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        method();
+
+        System.out.println("End of program");
+
+
+    }
+
+    public static void method() {
+        System.out.println("Method 1 Start");
+
+        method2();
+
+        System.out.println("Method 1 End");
+    }
+
+    public static void method2() {
+        System.out.println("Method 2 Start");
         Scanner sc = new Scanner(System.in);
 
         try {
@@ -13,12 +30,13 @@ public class Main {
             System.out.println(vect[position]);
         } catch (ArrayIndexOutOfBoundsException event) {
             System.out.println("Invalid position");
+            event.printStackTrace();
         } catch (InputMismatchException event) {
             System.out.println("Invalid character");
+            event.printStackTrace();
         }
 
-        System.out.println("End of program");
-
         sc.close();
+        System.out.println("Method 2 End");
     }
 }
